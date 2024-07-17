e6dba4d6d6639c1a0ef5dbcf94c55edb849b2dfe
Huy Hoang <huy.hoang@emandai.net>
2024-07-17 10:29:12 +0200

    update scenarios

--- scripts/sim/launch_mpc.sh
+++ scripts/sim/launch_mpc.sh
@@ -1,8 +1,8 @@
 python PRESET_3wrobot_NI.py \
-           --ctrl_mode MPC \
-           --pred_step_size_multiplier 8 \
-           --Nactor 4 \
-           --dt 0.2 \
-           --init_robot_pose_x 3 \
-           --init_robot_pose_y 1 \
-           --R1_diag 10 10 1 0 0 
+        --ctrl_mode MPC \
+        --pred_step_size_multiplier 8 \
+        --Nactor 4 \
+        --dt 0.2 \
+        --init_robot_pose_x 3 \
+        --init_robot_pose_y 1 \
+        --R1_diag 10 10 1 0 0 
---
+++ scripts/sim/launch_nominal.sh
@@ -0,0 +1,6 @@
+python PRESET_3wrobot_NI.py \
+        --ctrl_mode N_CTRL\
+        --init_robot_pose_x -3\
+        --init_robot_pose_y -3\
+        --init_robot_pose_theta 0.001\
+        --N_kappa "[0.5, 9, -2.5]"
